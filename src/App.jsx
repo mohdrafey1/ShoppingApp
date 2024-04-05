37eb7d734c1101a070c8403382ae310a1e02dd32
Mohd Rafey <mohdrafey600@gmail.com>
2024-04-05 16:19:00 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProductPage from './components/ProductPage';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     return (
         <>
             <ProductPage />
+            <Analytics />
         </>
     );
 }
