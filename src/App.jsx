f6ac197608aea6cf941eccc46785bf510b1a625f
Mohd Rafey <mohdrafey600@gmail.com>
2024-04-06 00:46:21 +0530

    added speed insights for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProductPage from './components/ProductPage';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
     return (
         <>
             <ProductPage />
             <Analytics />
+            <SpeedInsights />
         </>
     );
 }
