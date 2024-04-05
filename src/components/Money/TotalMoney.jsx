d666dbd096d4ae09fcda6239440c4b8ad58a85b4
Mohd Rafey <mohdrafey600@gmail.com>
2024-04-05 16:10:09 +0530

    total money adjust responiveness

--- src/components/Money/TotalMoney.jsx
+++ src/components/Money/TotalMoney.jsx
@@ -4,7 +4,7 @@ function TotalMoney({ totalMoney }) {
     let money = totalMoney * 80;
     return (
         <div className="w-full lg:w-1000 mb-4 bg-gradient-to-r from-green-500 to-teal-400 p-6 flex justify-center items-center">
-            <p className="text-center text-5xl font-serif text-white">
+            <p className="text-center text-3xl sm:text-5xl font-serif text-white">
                 {totalMoney <= 0
                     ? 'Money Ended'
                     : ` ${money.toLocaleString('en-IN', {
